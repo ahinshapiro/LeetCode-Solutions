0f92bc4afe53509fd704ff514d4b03b2fa1b4abc
Ahin Shapiro <38200327+ahinshapiro@users.noreply.github.com>
2021-06-23 15:28:32 +0530

    Create Gas Station.java

---
+++ Gas Station.java
@@ -0,0 +1,107 @@
+/*
+SampleTest Case that fails in the most optimised code by someother user.
+
+[ 204, 918, 18, 17, 35, 739, 913, 14, 76, 555, 333, 535, 653, 667, 52, 987, 422, 553, 599, 765, 494, 298, 16, 285, 272, 485, 989, 627, 422, 399, 258, 959, 475, 983, 535, 699, 663, 152, 606, 406, 173, 671, 559, 594, 531, 824, 898, 884, 491, 193, 315, 652, 799, 979, 890, 916, 331, 77, 650, 996, 367, 86, 767, 542, 858, 796, 264, 64, 513, 955, 669, 694, 382, 711, 710, 962, 854, 784, 299, 606, 655, 517, 376, 764, 998, 244, 896, 725, 218, 663, 965, 660, 803, 881, 482, 505, 336, 279 ]
+[ 273, 790, 131, 367, 914, 140, 727, 41, 628, 594, 725, 289, 205, 496, 290, 743, 363, 412, 644, 232, 173, 8, 787, 673, 798, 938, 510, 832, 495, 866, 628, 184, 654, 296, 734, 587, 142, 350, 870, 583, 825, 511, 184, 770, 173, 486, 41, 681, 82, 532, 570, 71, 934, 56, 524, 432, 307, 796, 622, 640, 705, 498, 109, 519, 616, 875, 895, 244, 688, 283, 49, 946, 313, 717, 819, 427, 845, 514, 809, 422, 233, 753, 176, 35, 76, 968, 836, 876, 551, 398, 12, 151, 910, 606, 932, 580, 795, 187 ]
+
+Sample optimised code:-
+class Solution {
+public:
+    int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
+
+       int start = gas.size()-1;
+       int end = 0;
+       int sum = gas[start] - cost[start];
+       while (start > end) {
+          if (sum >= 0) {
+             sum += gas[end] - cost[end];
+             ++end;
+          }
+          else {
+             --start;
+             sum += gas[start] - cost[start];
+          }
+       }
+       return sum >= 0 ? start : -1;
+    }
+};
+
+There are n gas stations along a circular route, where the amount of gas at the ith station is gas[i].
+
+You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from the ith station to its next (i + 1)th station. You begin the journey with an empty tank at one of the gas stations.
+
+Given two integer arrays gas and cost, return the starting gas station's index if you can travel around the circuit once in the clockwise direction, otherwise return -1. If there exists a solution, it is guaranteed to be unique
+
+ 
+
+Example 1:
+
+Input: gas = [1,2,3,4,5], cost = [3,4,5,1,2]
+Output: 3
+Explanation:
+Start at station 3 (index 3) and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
+Travel to station 4. Your tank = 4 - 1 + 5 = 8
+Travel to station 0. Your tank = 8 - 2 + 1 = 7
+Travel to station 1. Your tank = 7 - 3 + 2 = 6
+Travel to station 2. Your tank = 6 - 4 + 3 = 5
+Travel to station 3. The cost is 5. Your gas is just enough to travel back to station 3.
+Therefore, return 3 as the starting index.
+Example 2:
+
+Input: gas = [2,3,4], cost = [3,4,3]
+Output: -1
+Explanation:
+You can't start at station 0 or 1, as there is not enough gas to travel to the next station.
+Let's start at station 2 and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
+Travel to station 0. Your tank = 4 - 3 + 2 = 3
+Travel to station 1. Your tank = 3 - 3 + 3 = 3
+You cannot travel back to station 2, as it requires 4 unit of gas but you only have 3.
+Therefore, you can't travel around the circuit once no matter where you start.
+ 
+
+Constraints:
+
+gas.length == n
+cost.length == n
+1 <= n <= 104
+0 <= gas[i], cost[i] <= 104
+*/
+
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        for(int i = 0 ;i < gas.length ; i++){
+            if(gas[i] >= cost[i]){
+                if(getPath(gas,cost,i)) return i;
+            }
+        }
+        return -1;
+    }
+    public boolean getPath(int[] gas, int[] cost, int pos)
+    {
+        int path = 0;
+        int i = pos;
+        int j = 0;
+        path += gas[i++];
+        int flag = 0;
+        while(j <= gas.length)
+        {
+            flag ++;
+            if(i == gas.length){
+                i = 0;
+            }
+            if( i > 0 ){
+                if(path - cost[i-1] < 0) return false;
+                path = path - cost[i-1] + gas[i];
+            }
+            else{
+                if(path - cost[gas.length-1] < 0) return false;
+                path = path - cost[gas.length-1] + gas[i];
+            }
+            i++;
+            j++;
+        }
+        if( flag == gas.length+1 ) return true;
+        return false;
+    }
+}
+
